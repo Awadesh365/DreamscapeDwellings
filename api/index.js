ee3d0d235b876b6aa042def26748aed3a0693795
Awadesh <36awadesh@gmail.com>
2024-06-05 22:09:07 +0530

    Establish Database Connection
    Connected the application to the database to enable data storage and retrieval.

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
 /**
  * In order to use import instead of require, just go ahead and add [  "type": "module",] in the package.json
- * 
+ *   
  */
-
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-}
-)
+});
