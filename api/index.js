3b2d812cc0b7b130496bd0c4c5bdc94a515fc9d4
Awadesh <36awadesh@gmail.com>
2024-06-08 23:33:16 +0530

    Implement Sign-Up API Route
    Created the sign-up API route to handle user registration.

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -19,8 +20,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
