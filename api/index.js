64c0a94b8cab3906c389b438dc54e4cd87e0a0d5
Awadesh <36awadesh@gmail.com>
2024-06-08 23:24:17 +0530

    Create Test API Route
    Developed a test API route to verify server and API functionality.

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -21,3 +22,5 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRouter);
